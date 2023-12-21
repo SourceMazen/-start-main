6a2a3c31edb51571df4336012318a216d3f43698
SourceMazen <142892310+SourceMazen@users.noreply.github.com>
2023-12-21 07:03:03 +0200

    Add files via upload

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("Andromeda_3")
-        await app2.join_chat("Andromeda_3")
+        await app2.join_chat("https://t.me/FLS_45")
+        await app2.join_chat("https://t.me/Mr_Eirux")
     except:
         pass
 
